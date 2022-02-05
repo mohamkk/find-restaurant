150fb5c9ac43f261a166430c718e6dc7896f84fc
Yiseoyoon <aassdd38@naver.com>
2022-02-05 19:59:21 +0900

    0205 서윤 브랜치 확인용 커밋1

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# 불필요한 에러를 보여주지 않게 하는 os 설정
+
+from tensorflow.python.client import device_lib
+
+# print(device_lib.list_local_devices())
+# seoyoon 브랜치 커밋용 테스트 주석
+
